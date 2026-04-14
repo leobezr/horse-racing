7da9dc910099cd13890a58ab6829b63a4032044d
BEZR <le.bezr@gmail.com>
2026-04-14 20:43:41 +0100

    Refactor code structure for improved readability and maintainability

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import AppShell from './app/core/AppShell.vue'
 import { appRouter } from './app/core/router'
+import { appVuetify } from './app/core/plugins/vuetify'
 
 const app = createApp(AppShell)
 
 app.use(createPinia())
 app.use(appRouter)
+app.use(appVuetify)
 app.mount('#app')
