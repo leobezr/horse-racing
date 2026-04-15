ee8440d4e7395f6f3d0e0efde884585bd146d37c
BEZR <le.bezr@gmail.com>
2026-04-15 14:50:29 +0100

    fix: correct typo in parameter name of NumberNormalizer function

--- src/app/utils/number-normalizer.ts
+++ src/app/utils/number-normalizer.ts
@@ -1,5 +1,5 @@
 import { NUMBER_FORMAT } from "../../config/number";
 
-export const NumberNormalizer = (flatingNumber: number) => {
-  return new Intl.NumberFormat(NUMBER_FORMAT).format(flatingNumber);
+export const NumberNormalizer = (floatingNumber: number) => {
+  return new Intl.NumberFormat(NUMBER_FORMAT).format(floatingNumber);
 };
