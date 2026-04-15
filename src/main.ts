044f1e3f4939890ac1f5718d9530c662918ef500
BEZR <le.bezr@gmail.com>
2026-04-15 16:37:12 +0100

    fix: update import path for appRouter in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import AppShell from './app/core/AppShell.vue'
-import { appRouter } from './app/core/router'
+import { appRouter } from './app/routes/router'
 import { appVuetify } from './app/core/plugins/vuetify'
 
 const app = createApp(AppShell)
